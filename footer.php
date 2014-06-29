eea4db0029b5bab285bf351c56aba2dc161b3f68
Gabriel Trabasso <gabrieljt@gmail.com>
2014-06-29 13:45:30 -0300

    first commit

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php // section starts in header ?>
+</section>
+
+<footer>
+<?php // bones_page_navi(); ?>
+<?php the_breadcrumb(); ?>
+</footer>
+
+<?php // all js scripts are loaded in library/bones.php ?>
+<?php wp_footer(); ?>
+
+</body>
+
+</html> <!-- end of site. what a ride! -->
